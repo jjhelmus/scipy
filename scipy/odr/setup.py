671e0c3d62aacf1efc093dd7804b58514d8fa329
Jonathan Helmus <jjhelmus@gmail.com>
2017-05-01 20:56:05 -0500

    adjustments to odr

--- scipy/odr/setup.py
+++ scipy/odr/setup.py
@@ -10,23 +10,24 @@ def configuration(parent_package='', top_path=None):
     from numpy.distutils.system_info import get_info, BlasNotFoundError
     config = Configuration('odr', parent_package, top_path)
 
-    libodr_files = ['d_odr.f',
-                    'd_mprec.f',
-                    'dlunoc.f']
+    libodr_files = ['d_odr.c',
+                    'd_mprec.c',
+                    'dlunoc.c']
 
     blas_info = get_info('blas_opt')
     if blas_info:
-        libodr_files.append('d_lpk.f')
+        libodr_files.append('d_lpk.c')
     else:
         warnings.warn(BlasNotFoundError.__doc__)
-        libodr_files.append('d_lpkbls.f')
+        libodr_files.append('d_lpkbls.c')
 
     odrpack_src = [join('odrpack', x) for x in libodr_files]
     config.add_library('odrpack', sources=odrpack_src)
 
     sources = ['__odrpack.c']
-    libraries = ['odrpack'] + blas_info.pop('libraries', [])
+    libraries = ['odrpack'] + blas_info.pop('libraries', []) + ['f2c']
     include_dirs = ['.'] + blas_info.pop('include_dirs', [])
+    blas_info['library_dirs'].append('/home/jjhelmus')
     config.add_extension('__odrpack',
         sources=sources,
         libraries=libraries,
